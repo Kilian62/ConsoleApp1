83ff60a3a0eab2c344e54e4e30c7539212bcd642
votre-nom <dabovalkilian@gmail.com>
2025-03-26 16:57:39 +0100

    c'est zinzin visual alors que git pc ca pue

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,5 +5,15 @@ int sum (int a, int b)
  int sum = a + b;
     return sum;
 }
+
+bool test (int a, int b)
+{
+    return (a >=b)? true:false;
+}
+
 int res = sum(2 ,3);
-Console.WriteLine($"Le résultat est : {res} ");  
+Console.WriteLine($"Le résultat est : {res} ");
+
+bool testv = test(6, 3);
+
+Console.WriteLine($"It's {testv}");
