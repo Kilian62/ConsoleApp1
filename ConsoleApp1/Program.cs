a3370307ff4b8ae78058dab3bc83ebca2396a428
votre-nom <dabovalkilian@gmail.com>
2025-03-26 17:06:36 +0100

    la on parle du goat des amphis

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Clerentin le GOAT (ses amphis me manquent deja T_T )");
 int sum (int a, int b)
 {
  int sum = a + b;
@@ -11,9 +11,19 @@ bool test (int a, int b)
     return (a >=b)? true:false;
 }
 
+int oppo (int a)
+{
+    int res = a - (2 * a);
+    return res;
+}
+
 int res = sum(2 ,3);
-Console.WriteLine($"Le résultat est : {res} ");
+Console.WriteLine($"La somme de 2 et 3 est : {res} ");
 
 bool testv = test(6, 3);
 
 Console.WriteLine($"It's {testv}");
+
+int oppov = oppo(-7);
+
+Console.WriteLine($"Le res est inverse de 7 est {oppov}");
